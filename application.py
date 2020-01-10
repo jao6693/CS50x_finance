6d180d1d26e0d2db6736955d1cdcdd9aeb109f3b
jao6693 <fbeuret@gmail.com>
2020-01-10 10:30:24 +0000

    add register

--- application.py
+++ application.py
@@ -3,10 +3,10 @@ import os
 # from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
-from models import *
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from models import *
 
 from helpers import apology, login_required, lookup, usd
 
@@ -40,6 +40,7 @@ Session(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///finance.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # initialize app
+print("INITIALIZE")
 # https://stackoverflow.com/questions/9692962/flask-sqlalchemy-import-context-issue/9695045#9695045
 db.init_app(app)
 # push the context so that DB operations are performed inside an application context
@@ -55,24 +56,27 @@ if not os.environ.get("API_KEY"):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    print("INDEX")
+    return render_template("index.html")
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     """Buy shares of stock"""
+    print("BUY")
     return apology("TODO")
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
+    print("HISTORY")
     return apology("TODO")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
+    print("LOGIN")
     # forget any user_id
     session.clear()
 
@@ -87,16 +91,15 @@ def login():
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+        # query database for username using SQLAlchemy
+        user = User.query.filter_by(username=request.form.get("username")).first()
 
         # ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if user is None or not check_password_hash(user.hash, request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
         # remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = user.id
 
         # redirect user to home page
         return redirect("/")
@@ -108,7 +111,7 @@ def login():
 @app.route("/logout")
 def logout():
     """Log user out"""
-
+    print("LOGOUT")
     # forget any user_id
     session.clear()
 
@@ -119,12 +122,60 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
+    print("QUOTE")
     return apology("TODO")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    print("REGISTER")
+    # forget any user_id
+    session.clear()
+
+    # user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # query database for username using SQLAlchemy
+        user_exists = User.query.filter_by(username=request.form.get("username")).count()
+
+        # ensure username does not exist
+        if user_exists == 1:
+            return apology("username already exists", 403)
+
+        # ensure username is at least 3 characters long
+        if len(request.form.get("username")) < 3:
+            return apology("username must be at least 3 characters long", 403)
+
+        # ensure password is correct
+        if request.form.get("password") != request.form.get("password-confirm") :
+            return apology("passwords must be identical", 403)
+
+        # create the user based on the model provided
+        user = User(username=request.form.get("username"),hash=generate_password_hash(request.form.get("password")))
+        # add the user to generate an Id
+        db.session.add(user)
+        # commit changes
+        db.session.commit()
+        # retrieve the Id by querying the DB once again
+        user = User.query.filter_by(username=request.form.get("username")).first()
+
+        # remember which user has logged in
+        if user is not None:
+            session["user_id"] = user.id
+            # redirect user to home page
+            return redirect("/")
+
+    # user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
--- models.py
+++ models.py
@@ -3,9 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    """Master Data Table for Users"""
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, unique=True, nullable=False)
     hash = db.Column(db.Text, nullable=False)
     cash = db.Column(db.Float, nullable=False, default=10000.00)
+
+class Stock(db.Model):
+    """Master Data Table for Stocks"""
+    __tablename__ = "stocks"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    stock = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+
+class Transaction(db.Model):
+    """Transactional Table for Bought/Sold Stocks"""
+    __tablename__ = "transactions"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    stock_id = db.Column(db.Integer, db.ForeignKey("stock.id"), nullable=False)
+    user_id = person_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    currency = db.Column(db.Text, nullable=False, default="usd")
