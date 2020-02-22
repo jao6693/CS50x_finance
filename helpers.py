7f5368a4a5a3e3014e9d52e05f0f0525c1a31071
jao6693 <fbeuret@gmail.com>
2020-02-22 16:19:35 +0000

    modify logger call in helper

--- helpers.py
+++ helpers.py
@@ -40,7 +40,8 @@ def lookup(symbol, logger):
         api_key = os.environ.get("API_KEY")
         response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
-    except requests.RequestException as e:
+        logger.warning("Successfull API call")
+    except requests.RequestException:
         return None
 
     # parse response
