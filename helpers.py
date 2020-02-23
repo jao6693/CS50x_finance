7978729ec82b4cb0ddd9cfab9621b7a66cc566b7
jao6693 <fbeuret@gmail.com>
2020-02-23 17:42:57 +0000

    fix typo in helpers

--- helpers.py
+++ helpers.py
@@ -38,7 +38,7 @@ def lookup(symbol, logger):
     # contact API
     try:
         api_key = os.environ.get("API_KEY")
-        logger("API key used: ", api_key)
+        logger.info("API key used: ", api_key)
         response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException as e:
