d0a65a130ac0a2512e42498f346d836cec7366d7
jao6693 <fbeuret@gmail.com>
2020-02-24 14:20:07 +0000

    fix missing statement api_key

--- helpers.py
+++ helpers.py
@@ -37,6 +37,7 @@ def lookup(symbol):
 
     # contact API
     try:
+        api_key = os.environ.get("API_KEY")
         response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException as e:
